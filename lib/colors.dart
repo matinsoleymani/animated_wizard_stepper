87491fa510b9d3312124bc06225451441ec031d9
matinsoleymani <matinsoleymani07@gmail.com>
2024-12-31 15:40:18 +0330

    add comment for colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+//default color for filled items
 const primaryColor = Color.fromRGBO(18, 61, 161, 1);
-const disableColor = Color.fromRGBO(156, 163, 175, 1.0);
+//default color for disabled items
+const disableColor = Color.fromRGBO(156, 163, 175, 1.0); //default color for disabled items
+//default color for errors
 const errorColor = Color.fromRGBO(176, 0, 32, 1.0);
+//default color for texts
 const textColor = Color.fromRGBO(75, 85, 99, 1.0);
+//default color for filled items
 const primary600 = Color(0xff2d67d2);
+//gray color
 const gray400 = Color(0xff9ca3af);
+//gray color
 const gray500 = Color(0xff6b7280);
+//white color
 const white = Color(0xffffffff);
